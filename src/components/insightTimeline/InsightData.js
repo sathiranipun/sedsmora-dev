db68aa2dca1f8c0c7f21c1b616ff7e6fd279eccd
sathiranipun <sathiranipun.vsn@gmail.com>
2021-01-17 10:28:15 +0530

    insights update 01

--- src/components/insightTimeline/InsightData.js
+++ src/components/insightTimeline/InsightData.js
@@ -1,61 +1,94 @@
 export const InsightData = [
   {
-    title: "Meteo Shower",
-    description: `The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also`,
-    date: 'Jan 15, 2021',
+    title: "Quadrantid meteor shower 2021",
+    description: `The Quadrantid meteor shower will be active from 12 December to 12 January, producing its peak rate of meteors around 3 January.`,
+    date: 'Jan 03, 2021',
     month: 'jan',
-    date1: '15',
+    date1: '03',
     icon: 'falling-star', //falling-star, rocket, astronaut, clock,
     image: './images/carouselimages/carousel1.jpg'
   },
 
   {
-    title: "Meteo Shower",
-    description: `The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.`,
-    date: 'Jan 15, 2021',
+    title: "Moon at Last Quarter",
+    description: `The Moon will be prominent in the dawn sky, rising at around midnight.From Colombo, it will be visible in the dawn sky, rising at 00:13 (+0530) and reaching an altitude of 81° above the southern horizon before fading from view as dawn breaks around 06:11.`,
+    date: 'Jan 06, 2021',
     month: 'jan',
-    date1: '10',
-    icon: 'rocket', //falling-star, rocket, astronaut, clock
+    date1: '06',
+    icon: 'falling-star', //falling-star, rocket, astronaut, clock
     image: './images/carouselimages/carousel2.jpg'
   },
 
   {
-    title: "Meteo Shower",
-    description: `The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.`,
-    date: 'Jan 15, 2021',
+    title: "Conjunction of Mercury and Saturn",
+    description: `Mercury and Saturn will share the same right ascension, with Mercury passing 1°39' to the south of Saturn.
+
+    From Colombo, the pair will be difficult to observe as they will appear no higher than 8° above the horizon. They will become visible around 18:23 (+0530) as the dusk sky fades, 8° above your western horizon. They will then sink towards the horizon, setting 55 minutes after the Sun at 19:02.`,
+    date: 'Jan 10, 2021',
     month: 'jan',
-    date1: '15',
+    date1: '10',
     icon: 'falling-star', //falling-star, rocket, astronaut, clock
     image: './images/carouselimages/carousel3.jpg'
   },
 
   {
-    title: "Meteo Shower",
-    description: `The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.`,
-    date: 'Jan 15, 2021',
+    title: "Conjunction of Jupiter and Mercury",
+    description: `Jupiter and Mercury will share the same right ascension, with Jupiter passing 1°28' to the north of Mercury.
+
+    From Colombo, the pair will be difficult to observe as they will appear no higher than 9° above the horizon. They will become visible around 18:25 (+0530) as the dusk sky fades, 9° above your western horizon. They will then sink towards the horizon, setting 59 minutes after the Sun at 19:09.`,
+    date: 'Jan 11, 2021',
     month: 'jan',
-    date1: '15',
-    icon: 'astronaut', //falling-star, rocket, astronaut, clock
+    date1: '11',
+    icon: 'falling-star', //falling-star, rocket, astronaut, clock
     image: './images/carouselimages/carousel4.jpg'
   },
 
   {
-    title: "Meteo Shower",
-    description: `The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.`,
-    date: 'Jan 15, 2021',
+    title: "Conjunction of the Moon and Venus",
+    description: `The Moon and Venus will share the same right ascension, with the Moon passing 1°29' to the south of Venus. The Moon will be 28 days old.
+
+    From Colombo, the pair will be visible in the dawn sky, rising at 05:09 (+0530) – 1 hour and 16 minutes before the Sun – and reaching an altitude of 12° above the south-eastern horizon before fading from view as dawn breaks around 06:10.`,
+    date: 'Jan 12, 2021',
     month: 'jan',
-    date1: '15',
-    icon: 'clock', //falling-star, rocket, astronaut, clock
+    date1: '12',
+    icon: 'falling-star', //falling-star, rocket, astronaut, clock
     image: './images/carouselimages/carousel4.jpg'
   },
 
   {
-    title: "Meteo Shower",
+    title: "Conjunction of the Moon and Mercury",
     description: `The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.`,
-    date: 'Jan 15, 2021',
+    date: 'Jan 14, 2021',
     month: 'jan',
-    date1: '15',
-    icon: 'rocket', //falling-star, rocket, astronaut, clock
+    date1: '14',
+    icon: 'falling-star', //falling-star, rocket, astronaut, clock
     image: './images/carouselimages/carousel5.jpg'
   },
+
+  {
+    title: "γ-Ursae Minorid meteor shower 2021",
+    description: `The γ-Ursae Minorid meteor shower will be active from 15 January to 25 January, producing its peak rate of meteors around 19 January.
+
+    Over this period, there will be a chance of seeing γ-Ursae Minorid meteors whenever the shower's radiant point – in the constellation Ursa Minor – is above the horizon, with the number of visible meteors increasing the higher the radiant point is in the sky.`,
+    date: 'Jan 19, 2021',
+    month: 'jan',
+    date1: '19',
+    icon: 'falling-star', //falling-star, rocket, astronaut, clock
+    image: './images/carouselimages/carousel5.jpg'
+  },
+
+  
+  {
+    title: "Close approach of the Moon, Mars and Ura",
+    description: `The Moon, Mars and Uranus will make a close approach, passing within 4°38' of each other. The Moon will be 8 days old.
+
+    From Colombo, the trio will become visible around 18:29 (+0530) as the dusk sky fades, 82° above your northern horizon. They will then sink towards the horizon, setting at 00:37.`,
+    date: 'Jan 21, 2021',
+    month: 'jan',
+    date1: '21',
+    icon: 'falling-star', //falling-star, rocket, astronaut, clock
+    image: './images/carouselimages/carousel5.jpg'
+  },
+
+
 ]
