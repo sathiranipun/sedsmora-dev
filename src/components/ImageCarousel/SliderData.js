12ee06adbdf0ab4c6ec7f69a7f83be0d6f9c5721
nipunaluthge <nipunaluthge4815@gmail.com>
2021-01-17 18:17:35 +0530

    links changed

--- src/components/ImageCarousel/SliderData.js
+++ src/components/ImageCarousel/SliderData.js
@@ -1,17 +1,17 @@
 export const SliderData = [
   {
-    image: './images/carouselimages/carousel1.jpg'
+    image: '/images/carouselimages/carousel1.jpg'
   },
   {
-    image: './images/carouselimages/carousel2.jpg'  
+    image: '/images/carouselimages/carousel2.jpg'  
   },
   {
-    image: './images/carouselimages/carousel3.jpg'  
+    image: '/images/carouselimages/carousel3.jpg'  
   },
   {
-    image: './images/carouselimages/carousel4.jpg'  
+    image: '/images/carouselimages/carousel4.jpg'  
   },
   {
-    image: './images/carouselimages/carousel5.jpg'  
+    image: '/images/carouselimages/carousel5.jpg'  
   },
 ]
